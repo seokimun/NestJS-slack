c45db9b838e5e44f486b10adacbcac56a998db6d
seokimun <gimun08@naver.com>
2024-10-02 18:59:59 +0900

    feat: ✨ 포트 설정

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`${port}번 실행중...`);
 }
 bootstrap();
