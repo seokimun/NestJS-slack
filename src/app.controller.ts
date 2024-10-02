c9f619c9a87f88fdce2ddf659b3ec1e18dccc218
seokimun <gimun08@naver.com>
2024-10-02 20:02:06 +0900

    fix: 📝 Controller 기초

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('abc')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello') // GET /abc/hello
   getHello(): string {
     return this.appService.getHello();
   }
