01dc233757c54911fe3f9beaf8372ced0282af61
seokimun <gimun08@naver.com>
2024-10-02 21:07:50 +0900

    feat: ✨ ConfigModule 적용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('abc')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello') // GET /abc/hello
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
