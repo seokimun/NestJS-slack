7963c7c0ce540f05b7b7f5956d13ce19e589712f
seokimun <gimun08@naver.com>
2024-10-04 00:09:58 +0900

    refactor: ♻️ channels 및 workspaces API 설계

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 
-@Controller('workspaces')
-export class WorkspacesController {}
+@Controller('api/workspaces')
+export class WorkspacesController {
+    @Get()
+    getMyWorkspaces() {}
+
+    @Post()
+    createWorkspace() {}
+
+    @Get(':url/members')
+    getAllMembersFromWorkspace() {}
+
+    @Post(':url/members')
+    inviteMembersToWorkspace() {}
+
+    @Delete(':url/members/:id')
+    kickMemberFromWorkspace() {}
+
+    @Get(':url/members/:id')
+    DEPRECATE_getMemberInfoInWorkspace() {}
+}
