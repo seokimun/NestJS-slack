532385751f066e179011dab50bdd4e61f4c3d424
seokimun <gimun08@naver.com>
2024-10-03 22:10:27 +0900

    refactor: ♻️ JoinRequestDto 파일생성 및 세팅

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
     @Get()
     getUsers(@Req() req) {
         return req.user;
@@ -9,7 +13,7 @@ export class UsersController {
 
     @Post()
     postUsers(@Body() data: JoinRequestDto) {
-        
+        this.usersService.postUsers(data.email, data.nickname, data.password);
     }
 
     @Post('login')
